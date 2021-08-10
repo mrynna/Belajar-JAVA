4ea48582485812240773621728816953597a2ee3
mrynna <muhrianardiansah@gmail.com>
2021-08-10 20:47:05 +0800

    Menambahkan SC Deklarasi, Assignment, Variable

---
+++ 02 - variable, assignment dan deklarasi/src/com/variable/main.java
@@ -0,0 +1,25 @@
+package com.variable;
+
+public class main {
+    public static void main(String[] args) {
+
+    //Variable = tempat untuk menampung data (bisa diberi nama terserah kecuali keyword yang sudah ada di java)
+    int x =  10; // x adalah variable dan 10 adalah datanya
+    int y = 20; // y adalah variable dan 20 adalah datanya
+    
+    System.out.printf("\nnilai a = %d", x);
+    System.out.printf("\nNilai b = %d", y);
+
+    //Deklarasi = Untuk mendeklarasikan/memberitahukan kepada program bahwa ada sebuah variable
+    int a; // a adalah variable dan int adalah tipe datanya
+    float b; // b adalah variable dan float adalah tipe datanya
+
+    //Assignment = Untuk memberi pernyataan pada sebuah variable (memberi nilai pada sebuah variable)
+    a = 5;
+    b = 15;
+
+    System.out.printf("\nnilai a = %d", a);
+    System.out.printf("\nNilai b = %f", b);
+
+    }
+}
