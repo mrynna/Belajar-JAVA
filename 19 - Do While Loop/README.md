7866d20095c3f6b205738b5eb1f6fdf55ba194c6
mrynna <muhrianardiansah@gmail.com>
2021-08-26 21:34:01 +0800

    Menambahkan materi 19 - Do While Loop

---
+++ 19 - Do While Loop/README.md
@@ -0,0 +1,9 @@
+## Do While Loop
+
+Sebuah looping adalah versi dari while looping yang dimana aksinya akan dijalankan pertama kali dulu kemudian melihat kondsi untuk menjalankan aksi berikutnya
+
+## Folder Structure
+
+- `src`: Folder yang menyimpan file source code
+- `lib`: Folder yang menyimpan file dependencies
+- `bin`: Folder yang menyimpan output tercompile
---
+++ 19 - Do While Loop/src/com/dowhile/Main.java
@@ -0,0 +1,30 @@
+package com.dowhile;
+
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args) {
+        /*  
+        Adalah while loop yang dimana aksi akan dijalankan pertama kali terlebih dahulu kemudian melihat kondisi untuk menjalankan aksi berikutnya
+        Cara penggunaan : 
+                            do{
+                                statement aksi
+                            }while(kondisi);
+        */
+        Scanner masukan = new Scanner(System.in);
+        char kondisi;
+        int i = 1;
+
+        System.out.println("\n==Program Do While Loop Sederhana==");
+        System.out.println("\nAwal dari program\n");
+
+        do{
+            System.out.println("Aksi dijalankan yang ke-" + i++);
+            System.out.print("Masukkan huruf y jika ingin melanjutkan perulangan : ");
+            kondisi = masukan.next().charAt(0);
+        }while(kondisi == 'y' || kondisi == 'Y');
+
+        System.out.println("\nPerulangan berakhir");
+        System.out.println("Program berakhir");
+    }
+}
